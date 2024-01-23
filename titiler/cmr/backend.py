3c1e618eae45ef9c74a45e13282015dc3ca92169
vincentsarago <vincent.sarago@gmail.com>
2024-01-23 19:11:37 +0100

    start sketching the CMR mosaic backend

---
+++ titiler/cmr/backend.py
@@ -0,0 +1,227 @@
+"""TiTiler.cmr custom Mosaic Backend."""
+
+import itertools
+from typing import Any, Dict, List, Optional, Tuple, Type
+
+import attr
+import earthaccess
+from cachetools import TTLCache, cached
+from cachetools.keys import hashkey
+from cogeo_mosaic.backends import BaseBackend
+from cogeo_mosaic.errors import NoAssetFoundError
+from cogeo_mosaic.mosaic import MosaicJSON
+from morecantile import Tile, TileMatrixSet
+from rasterio.crs import CRS
+from rasterio.warp import transform_bounds
+from rio_tiler.constants import WEB_MERCATOR_TMS, WGS84_CRS
+from rio_tiler.io import Reader
+from rio_tiler.models import ImageData
+from rio_tiler.mosaic import mosaic_reader
+from rio_tiler.types import BBox
+
+from titiler.cmr.reader import ZarrReader
+from titiler.pgstac.settings import CacheSettings, RetrySettings
+from titiler.pgstac.utils import retry
+
+cache_config = CacheSettings()
+retry_config = RetrySettings()
+
+
+@attr.s
+class CMRBackend(BaseBackend):
+    """CMR Mosaic Backend."""
+
+    # ConceptID
+    input: str = attr.ib()
+
+    tms: TileMatrixSet = attr.ib(default=WEB_MERCATOR_TMS)
+    minzoom: int = attr.ib()
+    maxzoom: int = attr.ib()
+
+    # default values for bounds
+    bounds: BBox = attr.ib(default=(-180, -90, 180, 90))
+
+    crs: CRS = attr.ib(default=WGS84_CRS)
+    geographic_crs: CRS = attr.ib(default=WGS84_CRS)
+
+    # The reader is read-only (outside init)
+    mosaic_def: MosaicJSON = attr.ib(init=False)
+
+    # NOT USED
+    # Use reader (outside init)
+    reader: Type[Reader] = attr.ib(default=Reader, init=False)
+    reader_options: Dict = attr.ib(factory=dict, init=False)
+
+    _backend_name = "CMR"
+
+    def __attrs_post_init__(self) -> None:
+        """Post Init."""
+        # Construct a FAKE mosaicJSON
+        # mosaic_def has to be defined.
+        # we set `tiles` to an empty list.
+        self.mosaic_def = MosaicJSON(
+            mosaicjson="0.0.3",
+            name=self.input,
+            bounds=self.bounds,
+            minzoom=self.minzoom,
+            maxzoom=self.maxzoom,
+            tiles={},
+        )
+
+    @minzoom.default
+    def _minzoom(self):
+        return self.tms.minzoom
+
+    @maxzoom.default
+    def _maxzoom(self):
+        return self.tms.maxzoom
+
+    def write(self, overwrite: bool = True) -> None:
+        """This method is not used but is required by the abstract class."""
+        pass
+
+    def update(self) -> None:
+        """We overwrite the default method."""
+        pass
+
+    def _read(self) -> MosaicJSON:
+        """This method is not used but is required by the abstract class."""
+        pass
+
+    def assets_for_tile(self, x: int, y: int, z: int, **kwargs: Any) -> List[str]:
+        """Retrieve assets for tile."""
+        bbox = self.tms.bounds(Tile(x, y, z))
+        return self.get_assets(*bbox, **kwargs)
+
+    def assets_for_point(
+        self,
+        lng: float,
+        lat: float,
+        coord_crs: CRS = WGS84_CRS,
+        **kwargs: Any,
+    ) -> List[str]:
+        """Retrieve assets for point."""
+        raise NotImplementedError
+
+    def assets_for_bbox(
+        self,
+        xmin: float,
+        ymin: float,
+        xmax: float,
+        ymax: float,
+        coord_crs: CRS = WGS84_CRS,
+        **kwargs: Any,
+    ) -> List[Dict]:
+        """Retrieve assets for bbox."""
+        if coord_crs != WGS84_CRS:
+            xmin, ymin, xmax, ymax = transform_bounds(
+                coord_crs,
+                WGS84_CRS,
+                xmin,
+                ymin,
+                xmax,
+                ymax,
+            )
+
+        return self.get_assets(xmin, ymin, xmax, ymax, **kwargs)
+
+    @cached(  # type: ignore
+        TTLCache(maxsize=cache_config.maxsize, ttl=cache_config.ttl),
+        key=lambda self, xmin, ymin, xmax, ymax, **kwargs: hashkey(
+            self.input, str(xmin), str(ymin), str(xmax), str(ymax), **kwargs
+        ),
+    )
+    @retry(
+        tries=retry_config.retry,
+        delay=retry_config.delay,
+        exceptions=(),
+    )
+    def get_assets(
+        self,
+        xmin: float,
+        ymin: float,
+        xmax: float,
+        ymax: float,
+        limit: int = 100,
+        **kwargs: Any,
+    ) -> List[str]:
+        """Find assets."""
+        results = earthaccess.search_data(
+            concept_id=self.input,
+            bounding_box=(xmin, ymin, xmax, ymax),
+            count=limit,
+            **kwargs,
+        )
+        return list(
+            itertools.chain.from_iterable([res.data_links() for res in results])
+        )
+
+    @property
+    def _quadkeys(self) -> List[str]:
+        return []
+
+    def tile(
+        self,
+        tile_x: int,
+        tile_y: int,
+        tile_z: int,
+        cmr_query: Dict,
+        **kwargs: Any,
+    ) -> Tuple[ImageData, List[str]]:
+        """Get Tile from multiple observation."""
+        mosaic_assets = self.assets_for_tile(
+            tile_x,
+            tile_y,
+            tile_z,
+            **cmr_query,
+        )
+
+        if not mosaic_assets:
+            raise NoAssetFoundError(
+                f"No assets found for tile {tile_z}-{tile_x}-{tile_y}"
+            )
+
+        def _reader(src_path: str, x: int, y: int, z: int, **kwargs: Any) -> ImageData:
+            if src_path.endswith(".tif"):
+                reader = Reader
+            else:
+                reader = ZarrReader
+
+            with reader(src_path, tms=self.tms) as src_dst:
+                return src_dst.tile(x, y, z, **kwargs)
+
+        return mosaic_reader(mosaic_assets, _reader, tile_x, tile_y, tile_z, **kwargs)
+
+    def point(
+        self,
+        lon: float,
+        lat: float,
+        cmr_query: Dict,
+        coord_crs: CRS = WGS84_CRS,
+        **kwargs: Any,
+    ) -> List:
+        """Get Point value from multiple observation."""
+        raise NotImplementedError
+
+    def part(
+        self,
+        bbox: BBox,
+        cmr_query: Dict,
+        dst_crs: Optional[CRS] = None,
+        bounds_crs: CRS = WGS84_CRS,
+        **kwargs: Any,
+    ) -> Tuple[ImageData, List[str]]:
+        """Create an Image from multiple items for a bbox."""
+        raise NotImplementedError
+
+    def feature(
+        self,
+        shape: Dict,
+        cmr_query: Dict,
+        dst_crs: Optional[CRS] = None,
+        shape_crs: CRS = WGS84_CRS,
+        max_size: int = 1024,
+        **kwargs: Any,
+    ) -> Tuple[ImageData, List[str]]:
+        """Create an Image from multiple items for a GeoJSON feature."""
+        raise NotImplementedError
---
+++ titiler/cmr/reader.py
@@ -0,0 +1,279 @@
+"""ZarrReader.
+
+Originaly from titiler-xarray
+"""
+
+import contextlib
+import pickle
+import re
+from typing import Any, Dict, List, Optional
+
+import attr
+import fsspec
+import numpy
+import s3fs
+import xarray
+from cachetools import TTLCache
+from morecantile import TileMatrixSet
+from rasterio.crs import CRS
+from rio_tiler.constants import WEB_MERCATOR_TMS, WGS84_CRS
+from rio_tiler.io.xarray import XarrayReader
+from rio_tiler.types import BBox
+
+from titiler.pgstac.settings import CacheSettings
+
+# Use simple in-memory cache for now (we can switch to redis later)
+cache_config = CacheSettings()
+cache_client: Any = TTLCache(maxsize=cache_config.maxsize, ttl=cache_config.ttl)
+
+
+def parse_protocol(src_path: str, reference: Optional[bool] = False):
+    """
+    Parse protocol from path.
+    """
+    match = re.match(r"^(s3|https|http)", src_path)
+    protocol = "file"
+    if match:
+        protocol = match.group(0)
+
+    # override protocol if reference
+    if reference:
+        protocol = "reference"
+
+    return protocol
+
+
+def xarray_engine(src_path: str):
+    """
+    Parse xarray engine from path.
+    """
+    #  ".hdf", ".hdf5", ".h5" will be supported once we have tests + expand the type permitted for the group parameter
+    H5NETCDF_EXTENSIONS = [".nc", ".nc4"]
+    lower_filename = src_path.lower()
+    if any(lower_filename.endswith(ext) for ext in H5NETCDF_EXTENSIONS):
+        return "h5netcdf"
+    else:
+        return "zarr"
+
+
+def get_filesystem(
+    src_path: str,
+    protocol: str,
+    xr_engine: str,
+    anon: bool = True,
+):
+    """
+    Get the filesystem for the given source path.
+    """
+    if protocol == "s3":
+        s3_filesystem = s3fs.S3FileSystem()
+        return (
+            s3_filesystem.open(src_path)
+            if xr_engine == "h5netcdf"
+            else s3fs.S3Map(root=src_path, s3=s3_filesystem)
+        )
+
+    elif protocol == "reference":
+        reference_args = {"fo": src_path, "remote_options": {"anon": anon}}
+        return fsspec.filesystem("reference", **reference_args).get_mapper("")
+
+    elif protocol in ["https", "http", "file"]:
+        filesystem = fsspec.filesystem(protocol)  # type: ignore
+        return (
+            filesystem.open(src_path)
+            if xr_engine == "h5netcdf"
+            else filesystem.get_mapper(src_path)
+        )
+
+    else:
+        raise ValueError(f"Unsupported protocol: {protocol}")
+
+
+def xarray_open_dataset(
+    src_path: str,
+    group: Optional[Any] = None,
+    reference: Optional[bool] = False,
+    decode_times: Optional[bool] = True,
+    consolidated: Optional[bool] = True,
+) -> xarray.Dataset:
+    """Open dataset."""
+    # Generate cache key and attempt to fetch the dataset from cache
+    cache_key = f"{src_path}_{group}" if group is not None else src_path
+    data_bytes = cache_client.get(cache_key, None)
+    if data_bytes:
+        return pickle.loads(data_bytes)
+
+    protocol = parse_protocol(src_path, reference=reference)
+    xr_engine = xarray_engine(src_path)
+    file_handler = get_filesystem(src_path, protocol, xr_engine)
+
+    # Arguments for xarray.open_dataset
+    # Default args
+    xr_open_args: Dict[str, Any] = {
+        "decode_coords": "all",
+        "decode_times": decode_times,
+    }
+
+    # Argument if we're opening a datatree
+    if isinstance(group, int):
+        xr_open_args["group"] = group
+
+    # NetCDF arguments
+    if xr_engine == "h5netcdf":
+        xr_open_args["engine"] = "h5netcdf"
+        xr_open_args["lock"] = False
+
+    else:
+        # Zarr arguments
+        xr_open_args["engine"] = "zarr"
+        xr_open_args["consolidated"] = consolidated
+
+    # Additional arguments when dealing with a reference file.
+    if reference:
+        xr_open_args["consolidated"] = False
+        xr_open_args["backend_kwargs"] = {"consolidated": False}
+
+    ds = xarray.open_dataset(file_handler, **xr_open_args)
+
+    # Serialize the dataset to bytes using pickle
+    cache_client[cache_key] = pickle.dumps(ds)
+
+    return ds
+
+
+def arrange_coordinates(da: xarray.DataArray) -> xarray.DataArray:
+    """
+    Arrange coordinates to DataArray.
+    An rioxarray.exceptions.InvalidDimensionOrder error is raised if the coordinates are not in the correct order time, y, and x.
+    See: https://github.com/corteva/rioxarray/discussions/674
+    We conform to using x and y as the spatial dimension names. You can do this a bit more elegantly with metpy but that is a heavy dependency.
+    """
+    if "x" not in da.dims and "y" not in da.dims:
+        latitude_var_name = "lat"
+        longitude_var_name = "lon"
+        if "latitude" in da.dims:
+            latitude_var_name = "latitude"
+        if "longitude" in da.dims:
+            longitude_var_name = "longitude"
+        da = da.rename({latitude_var_name: "y", longitude_var_name: "x"})
+    if "time" in da.dims:
+        da = da.transpose("time", "y", "x")
+    else:
+        da = da.transpose("y", "x")
+    return da
+
+
+def get_variable(
+    ds: xarray.Dataset,
+    variable: str,
+    time_slice: Optional[str] = None,
+    drop_dim: Optional[str] = None,
+) -> xarray.DataArray:
+    """Get Xarray variable as DataArray."""
+    da = ds[variable]
+    da = arrange_coordinates(da)
+    # TODO: add test
+    if drop_dim:
+        dim_to_drop, dim_val = drop_dim.split("=")
+        da = da.sel({dim_to_drop: dim_val}).drop(dim_to_drop)
+    da = arrange_coordinates(da)
+
+    if (da.x > 180).any():
+        # Adjust the longitude coordinates to the -180 to 180 range
+        da = da.assign_coords(x=(da.x + 180) % 360 - 180)
+
+        # Sort the dataset by the updated longitude coordinates
+        da = da.sortby(da.x)
+
+    # Make sure we have a valid CRS
+    crs = da.rio.crs or "epsg:4326"
+    da.rio.write_crs(crs, inplace=True)
+
+    if "time" in da.dims:
+        if time_slice:
+            time_as_str = time_slice.split("T")[0]
+            if da["time"].dtype == "O":
+                da["time"] = da["time"].astype("datetime64[ns]")
+            da = da.sel(
+                time=numpy.array(time_as_str, dtype=numpy.datetime64), method="nearest"
+            )
+        else:
+            da = da.isel(time=0)
+
+    return da
+
+
+@attr.s
+class ZarrReader(XarrayReader):
+    """ZarrReader: Open Zarr file and access DataArray."""
+
+    src_path: str = attr.ib()
+    variable: str = attr.ib()
+
+    # xarray.Dataset options
+    reference: bool = attr.ib(default=False)
+    decode_times: bool = attr.ib(default=False)
+    group: Optional[Any] = attr.ib(default=None)
+    consolidated: Optional[bool] = attr.ib(default=True)
+
+    # xarray.DataArray options
+    time_slice: Optional[str] = attr.ib(default=None)
+    drop_dim: Optional[str] = attr.ib(default=None)
+
+    tms: TileMatrixSet = attr.ib(default=WEB_MERCATOR_TMS)
+    geographic_crs: CRS = attr.ib(default=WGS84_CRS)
+
+    ds: xarray.Dataset = attr.ib(init=False)
+    input: xarray.DataArray = attr.ib(init=False)
+
+    bounds: BBox = attr.ib(init=False)
+    crs: CRS = attr.ib(init=False)
+
+    _minzoom: int = attr.ib(init=False, default=None)
+    _maxzoom: int = attr.ib(init=False, default=None)
+
+    _dims: List = attr.ib(init=False, factory=list)
+    _ctx_stack = attr.ib(init=False, factory=contextlib.ExitStack)
+
+    def __attrs_post_init__(self):
+        """Set bounds and CRS."""
+        self.ds = self._ctx_stack.enter_context(
+            xarray_open_dataset(
+                self.src_path,
+                group=self.group,
+                reference=self.reference,
+                consolidated=self.consolidated,
+            ),
+        )
+        self.input = get_variable(
+            self.ds,
+            self.variable,
+            time_slice=self.time_slice,
+            drop_dim=self.drop_dim,
+        )
+
+        self.bounds = tuple(self.input.rio.bounds())
+        self.crs = self.input.rio.crs
+
+        self._dims = [
+            d
+            for d in self.input.dims
+            if d not in [self.input.rio.x_dim, self.input.rio.y_dim]
+        ]
+
+    @classmethod
+    def list_variables(
+        cls,
+        src_path: str,
+        group: Optional[Any] = None,
+        reference: Optional[bool] = False,
+        consolidated: Optional[bool] = True,
+    ) -> List[str]:
+        """List available variable in a dataset."""
+        with xarray_open_dataset(
+            src_path,
+            group=group,
+            reference=reference,
+            consolidated=consolidated,
+        ) as ds:
+            return list(ds.data_vars)  # type: ignore
