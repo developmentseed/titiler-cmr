6678938d9c8aa70c4315c985e8917e407313d5fb
vincentsarago <vincent.sarago@gmail.com>
2024-02-01 15:19:32 +0100

    revert to python 3.10

--- infrastructure/aws/cdk/app.py
+++ infrastructure/aws/cdk/app.py
@@ -40,7 +40,7 @@ class LambdaStack(Stack):
         id: str,
         memory: int = 1024,
         timeout: int = 30,
-        runtime: aws_lambda.Runtime = aws_lambda.Runtime.PYTHON_3_11,
+        runtime: aws_lambda.Runtime = aws_lambda.Runtime.PYTHON_3_10,
         concurrent: Optional[int] = None,
         permissions: Optional[List[iam.PolicyStatement]] = None,
         environment: Optional[Dict] = None,
