36455f2e1c2e36f1a872c82e31488624746e4e85
Aimee Barciauskas <aimee@developmentseed.org>
2025-05-22 17:12:21 -0700

    Precommit fixes

--- infrastructure/aws/cdk/config.py
+++ infrastructure/aws/cdk/config.py
@@ -1,17 +1,17 @@
 """STACK Configs."""
 
-from typing import Dict, List, Optional, Annotated
+from typing import Dict, List, Optional
 
-from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class StackSettings(BaseSettings):
     """Stack settings"""
+
     veda_custom_host: Optional[str] = None
 
-    model_config = SettingsConfigDict(
-        env_file=".env", extra="ignore"
-    )
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
 
 class AppSettings(BaseSettings):
     """Application settings"""
