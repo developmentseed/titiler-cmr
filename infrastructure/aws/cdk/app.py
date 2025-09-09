9944c7cc05780d9bb19d58e2c6f073e9daa93553
hrodmn <henry.rodman@gmail.com>
2025-09-09 15:42:55 -0500

    remove leftover env var

--- infrastructure/aws/cdk/app.py
+++ infrastructure/aws/cdk/app.py
@@ -20,7 +20,6 @@ stack_settings, app_settings = StackSettings(), AppSettings()
 DEFAULT_ENV = {
     "AWS_LAMBDA_LOG_FORMAT": "JSON",
     "AWS_LAMBDA_LOG_LEVEL": "INFO",
-    "AWS_LAMBDA_EXEC_WRAPPER": "/opt/otel-instrument",
     "GDAL_CACHEMAX": "200",  # 200 mb
     "GDAL_DISABLE_READDIR_ON_OPEN": "EMPTY_DIR",
     "GDAL_INGESTED_BYTES_AT_OPEN": "32768",  # get more bytes when opening the files.
