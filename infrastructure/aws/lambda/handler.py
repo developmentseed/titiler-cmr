4cf942fc4677f2d58d57689064d7c9564aa450a5
Chuck Daniels <chuck@developmentseed.org>
2025-12-11 16:25:33 -0500

    Remove deprecated FastAPI on_event decorator.
    The `lifespan` function in `main.py` takes the place of the function
    removed in this commit, but it seems that when `lifespan` was added, the
    now-removed function was not removed, as it should have been.

--- infrastructure/aws/lambda/handler.py
+++ infrastructure/aws/lambda/handler.py
@@ -4,14 +4,13 @@ import asyncio
 import logging
 import os
 import warnings
-from typing import Any, Dict
+from typing import Any
 
-import earthaccess
 from mangum import Mangum
+from mangum.types import LambdaContext, LambdaEvent
 
 from titiler.cmr.logger import configure_logging
 from titiler.cmr.main import app
-from titiler.cmr.settings import AuthSettings
 
 configure_logging()
 
@@ -19,22 +18,10 @@ warnings.filterwarnings("ignore", category=UserWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
 logging.getLogger("numexpr").setLevel(logging.WARNING)
 
-auth_config = AuthSettings()
-
-
-@app.on_event("startup")
-async def startup_event() -> None:
-    """startup."""
-    if auth_config.strategy == "environment":
-        app.state.cmr_auth = earthaccess.login(strategy="environment")
-    else:
-        app.state.cmr_auth = None
-
 
 handler = Mangum(
     app,
     lifespan="off",
-    api_gateway_base_path=None,
     text_mime_types=[
         "application/json",
         "application/javascript",
@@ -48,6 +35,6 @@ if "AWS_EXECUTION_ENV" in os.environ:
     loop.run_until_complete(app.router.startup())
 
 
-def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
+def lambda_handler(event: LambdaEvent, context: LambdaContext) -> dict[str, Any]:
     """Lambda handler with container-specific optimizations and OTEL tracing."""
     return handler(event, context)
